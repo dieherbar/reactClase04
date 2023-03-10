cabffb77b91de39e02bba6fb815a3632f69eea9a
Diego Barletta <diegohbarletta@gmail.com>
2023-03-10 00:34:09 -0300

    commit03

--- js/app.js
+++ js/app.js
@@ -15,14 +15,32 @@ const myCat = {
 console.log(myDog);
 console.log(myCat);
 
-class Mouse {
-    constructor(name,age){
-        this.name = name;
-        this.age = age;
-    }
+// class Mouse {
+//     constructor(name, age){
+//         this.nombre = name; //this. -->puede tener cualquier valor
+//         this.age = age;
+//     }
+// }
+
+function Mouse(name,age){
+    this.name = name;
+    this.age = age;
 }
 
-const myMouse = new Mouse("simon",3)
+const myMouse = new Mouse("simon",3);
+console.log(myMouse);
+
+class Input{
+    constructor(parentId,text){
+        this.parentId = parentId;
+        this.text = text;
+    }
+    render(){
+        let sitio = document.getElementById(this.parentId);
+        let inputHtml ="<input type='email'  id='email' placeholder='Ingrese email'>";
+        sitio.innerHTML = inputHtml;
+    }
+}
 
 class Boton{
     constructor(parentId,text){
@@ -31,9 +49,24 @@ class Boton{
     }
     render(){
         let sitio = document.getElementById(this.parentId);
-        sitio.parentId="boton";
-        sitio.text="boton JS";
+        let botonHtml ="<button>Boton JS</button>";
+        sitio.innerHTML = botonHtml;
+        // sitio.parentId="boton";
+        // sitio.text="boton JS";
     }
 }
-const myBoton = new Boton();
+function botonCancelar(parentId,text){
+    this.parentId = parentId;
+    this.text = text;
+    let sitio = document.getElementById(this.parentId);
+    let botonHtml ="<button>Cancelar JS</button>";
+    sitio.innerHTML = botonHtml;    
+}
+
+const unInput = new Input("input");
+unInput.render();
+
+const myBoton = new Boton("boton");
 myBoton.render();
+
+const cancelar = new botonCancelar("botonC","Cancelar JS");
